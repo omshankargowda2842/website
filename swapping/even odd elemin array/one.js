2b2a8c0115e5a8a30e456c95e374df0bd3153e7a
Omshankar <omshankarbombali1215@gmail.com>
2022-08-13 11:08:01 +0530

    ok

---
+++ swapping/even odd elemin array/one.js
@@ -0,0 +1,12 @@
+let a=[45,56,34,6,57,87,3]
+var b=[]
+let c=[]
+for(i=0;i<a.length;i++){
+   if(a[i]%2==0){
+     b.push(a[i]) 
+       
+   }
+     else{c.push(a[i])}
+} 
+console.log(b)
+console.log(c)
